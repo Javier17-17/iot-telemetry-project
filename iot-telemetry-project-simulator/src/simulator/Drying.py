8e10d4f51d3784acf594953d559071be6a127e6d
Eduardo44817 <edupatata7@gmail.com>
2026-05-29 13:17:58 +0200

    Usar fecha actual en el simulador

--- iot-telemetry-project-simulator/src/simulator/Drying.py
+++ iot-telemetry-project-simulator/src/simulator/Drying.py
@@ -1,16 +1,15 @@
 import random
-from datetime import datetime, timedelta
+from datetime import datetime
 import uuid
 
+
 class Drying:
-    _device_id= None
+    _device_id = None
     _temperature = None
     _humidity = None
     _timestamp = None
 
-
     # CONSTRUCTOR
-
     def __init__(self):
         self._device_id = str(uuid.uuid4())
         self._temperature = random.randint(1, 25)
@@ -19,7 +18,7 @@ class Drying:
 
         print(self._device_id)
         print(self._temperature, "º")
-        print(self._humidity,"%")
+        print(self._humidity, "%")
         print(self._timestamp)
 
     # GETTERS
@@ -32,18 +31,6 @@ class Drying:
     def get_date_time_drying(self):
         return self._timestamp
 
-    # GENERAR FECHA Y HORA RANDOM
-
+    # GENERAR FECHA Y HORA ACTUAL
     def timestamp_random(self):
-        initial_date = datetime(2010, 1, 1)
-        end_date = datetime.now()
-        delta = end_date - initial_date
-
-        date_random = random.randint(0,delta.days)
-        hour_random = random.randint(0,86400)
-
-        final = initial_date + timedelta(days = date_random, hours = hour_random)
-        return final.strftime('%Y-%m-%d %H:%M:%S')
-
-
-
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
--- iot-telemetry-project-simulator/src/simulator/Scale.py
+++ iot-telemetry-project-simulator/src/simulator/Scale.py
@@ -1,6 +1,7 @@
 import random
-from datetime import datetime, timedelta
+from datetime import datetime
 import uuid
+
 
 class Scale:
     _device_id = None
@@ -8,48 +9,31 @@ class Scale:
     _net_weight = None
     _timestamp = None
 
-# CONSTRUCTOR
-
+    # CONSTRUCTOR
     def __init__(self):
-
         self._device_id = str(uuid.uuid4())
-        #self._gross_weight = random.uniform(1.50, 10.50)
-        #self._net_weight = random.uniform(1.50, self._gross_weight)
-        #self._timestamp = str(self.timestamp_random())
         self.weigh_again()
 
-# GETTERS
+    # GETTERS
     def get_gross_weight(self):
         return self._gross_weight
 
     def get_net_weight(self):
         return self._net_weight
 
-# PULSAR BOTÓN DE TARA
-
+    # PULSAR BOTON DE TARA
     def press_tare(self):
         self._gross_weight = 0
         self._net_weight = 0
 
-        print("báscula reiniciada")
+        print("bascula reiniciada")
 
-# VOLVER A PESAR EN LA MISMA BÁSCULA
-
+    # VOLVER A PESAR EN LA MISMA BASCULA
     def weigh_again(self):
-
         self._gross_weight = random.uniform(1.50, 10.50)
         self._net_weight = random.uniform(1.50, self._gross_weight)
         self._timestamp = str(self.timestamp_random())
 
-# GENERAR FECHA Y HORA RANDOM
-
+    # GENERAR FECHA Y HORA ACTUAL
     def timestamp_random(self):
-        initial_date = datetime(2010, 1, 1)
-        end_date = datetime.now()
-        delta = end_date - initial_date
-
-        date_random = random.randint(0,delta.days)
-        hour_random = random.randint(0,86400)
-
-        final = initial_date + timedelta(days = date_random, hours = hour_random)
-        return final.strftime('%Y-%m-%d %H:%M:%S')
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
